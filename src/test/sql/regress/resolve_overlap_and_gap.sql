79aea014562bf290b77e3aa18063391b91bbd812
Lars Aksel Opsahl <lop@skogoglandskap.no>
2020-01-11 09:06:14 +0100

    Added more tests

--- src/test/sql/regress/resolve_overlap_and_gap.sql
+++ src/test/sql/regress/resolve_overlap_and_gap.sql
@@ -7,6 +7,10 @@ CREATE table test_data.overlap_gap_input_t2 AS (SELECT * from test_data.overlap_
 -- Call function to resolve overlap and gap in the function in test_data.overlap_gap_input_t1 which we just testet for overlap
 CALL resolve_overlap_gap_run('test_data.overlap_gap_input_t2','geom',4258,'test_data.overlap_gap_input_t2_res','test_topo_t2',5,4);
 
+SELECT 'check_border_lines', count(geo) from topo_update.border_line_segments;
+
+SELECT 'check_added_lines', count(geom) from test_topo_t2.edge;
+
 SELECT topology.droptopology('test_topo_t2');
 SELECT topology.droptopology('test_topo_t2_1');
 SELECT topology.droptopology('test_topo_t2_2');

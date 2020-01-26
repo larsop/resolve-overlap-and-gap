c84a706f5a045ea44275ead4349e5726ebab2668
Lars Aksel Opsahl <lop@skogoglandskap.no>
2020-01-26 13:38:02 +0100

    Break up and and single lines

--- src/main/extern_pgtopo_update_sql/utils/function_03_add_border_lines.sql
+++ src/main/extern_pgtopo_update_sql/utils/function_03_add_border_lines.sql
@@ -1,10 +1,3 @@
-/**
- * Add border lines 
- */
-DROP FUNCTION topo_update.add_border_lines (_topology_name character varying, _new_line geometry, _snap_tolerance float, _table_name_result_prefix varchar);
-
--- TODO add code for simple_add_v2
--- TODO add code for get_single_lineparts
 
 CREATE OR REPLACE FUNCTION topo_update.add_border_lines (_topology_name character varying, _new_line geometry, _snap_tolerance float, _table_name_result_prefix varchar)
   RETURNS integer
@@ -13,79 +6,80 @@ DECLARE
   update_egde_id int;
   old_egde_geom geometry;
   new_egde_geom geometry;
+  single_line_geo geometry	;
   command_string text;
   v_state TEXT;
   v_msg TEXT;
   v_detail TEXT;
   v_hint TEXT;
   v_context TEXT;
+  i INT DEFAULT 0; 
+  dim INT DEFAULT 0;  
+  no_cutline_filename varchar;
 BEGIN
+  no_cutline_filename = _table_name_result_prefix || '_no_cut_line_failed';	
   BEGIN
-    update_egde_id = 0;
-    command_string := Format('SELECT e.edge_id, e.geom FROM %1$s.edge_data e where e.geom && %2$L and ST_Intersects(e.geom,%2$L) limit 1', _topology_name, _new_line);
-    -- RAISE NOTICE '%s, command_string test %s', ST_Length (_new_line), command_string;
-    --		SELECT e.edge_id, e.geom FROM test_topo.edge_data e where e.geom && 0102000020A2100000020000007E2ED15B6788194000E6ABFBFF314E40F0D5450C668819405156FE0700324E40 and ST_Intersects(e.geom,0102000020A2100000020000007E2ED15B6788194000E6ABFBFF314E40F0D5450C668819405156FE0700324E40) limit 1
-    EXECUTE command_string INTO update_egde_id, old_egde_geom;
-    IF update_egde_id IS NULL THEN
-      new_egde_geom := _new_line;
-      --			RAISE NOTICE 'new topo edge wil lbe added no intersecion found';
-    ELSE
-      --			RAISE NOTICE 'Intersectoin found with edge_id %', update_egde_id;
-      --			SELECT ST_Union(e.geom,_new_line)
-      --			from topo_ar5_forest_sysdata.edge_data e
-      --			where e.geom && _new_line
-      --			and ST_Intersects(e.geom,_new_line)
-      --			into old_egde_geom;
-      --			new_egde_geom := ST_LineMerge(old_egde_geom);
-      --			IF ST_geometryType(new_egde_geom) = 'ST_LineString'  THEN
-      -- 				RAISE NOTICE 'update edge id %, with %', update_egde_id,ST_AsText(new_egde_geom);
-      --				perform topology.ST_ChangeEdgeGeom(
-      --				'topo_ar5_forest_sysdata', update_egde_id , new_egde_geom) ;
-      --				new_egde_geom := null;
-      --			ELSE
-      --				RAISE NOTICE 'new topo edge will be added no single linstring added';
-      new_egde_geom := _new_line;
-      --			END IF;
-    END IF;
-    IF new_egde_geom IS NOT NULL THEN
-      command_string := Format('select topology.TopoGeo_addLinestring(%s,%L,%s)', Quote_literal(_topology_name), new_egde_geom, _snap_tolerance);
-      EXECUTE command_string;
-    END IF;
+	command_string := Format('select topology.TopoGeo_addLinestring(%s,%L,%s)', Quote_literal(_topology_name), _new_line, _snap_tolerance);
+    EXECUTE command_string;
     EXCEPTION
     WHEN OTHERS THEN
       RAISE NOTICE 'failed topo_update.add_border_lines ::::::::::::::::::::::::::::::::::::::::::::::::::: % for edge_id % ', ST_GeometryType (new_egde_geom), update_egde_id;
-    GET STACKED DIAGNOSTICS v_state = RETURNED_SQLSTATE, v_msg = MESSAGE_TEXT, v_detail = PG_EXCEPTION_DETAIL, v_hint = PG_EXCEPTION_HINT,
-    v_context = PG_EXCEPTION_CONTEXT;
-    RAISE NOTICE 'failed: state  : % message: % detail : % hint   : % context: %', v_state, v_msg, v_detail, v_hint, v_context;
-    EXECUTE Format('INSERT INTO %s(line_geo_lost, error_info, d_state, d_msg, d_detail, d_hint, d_context, geo) VALUES(%L, %L, %L, %L, %L, %L, %L, %L)', _table_name_result_prefix || '_no_cut_line_failed', FALSE, 'Warning, will do retry ', v_state, v_msg, v_detail, v_hint, v_context, new_egde_geom);
-    -- ERROR:  XX000: SQL/MM Spatial exception - coincident node
-    BEGIN
-	  PERFORM topology.TopoGeo_addLinestring(_topology_name,geom,_snap_tolerance) 
-      FROM (
-        SELECT (ST_Dump (geom)).geom
-        FROM (
-          SELECT topo_update.get_single_lineparts ((ST_Dump (_new_line)).geom) AS geom) AS r) AS r
-        WHERE ST_length (geom) > 0;
-      EXCEPTION
-      WHEN OTHERS THEN
-        GET STACKED DIAGNOSTICS v_state = RETURNED_SQLSTATE, v_msg = MESSAGE_TEXT, v_detail = PG_EXCEPTION_DETAIL, v_hint = PG_EXCEPTION_HINT,
-        v_context = PG_EXCEPTION_CONTEXT;
+      GET STACKED DIAGNOSTICS v_state = RETURNED_SQLSTATE, v_msg = MESSAGE_TEXT, v_detail = PG_EXCEPTION_DETAIL, v_hint = PG_EXCEPTION_HINT,
+      v_context = PG_EXCEPTION_CONTEXT;
       RAISE NOTICE 'failed: state  : % message: % detail : % hint   : % context: %', v_state, v_msg, v_detail, v_hint, v_context;
-      EXECUTE Format('INSERT INTO %s(line_geo_lost, error_info, d_state, d_msg, d_detail, d_hint, d_context, geo) VALUES(%L, %L, %L, %L, %L, %L, %L, %L)', _table_name_result_prefix || '_no_cut_line_failed', FALSE, 'Failed topo_update.add_border_lines ', v_state, v_msg, v_detail, v_hint, v_context, _new_line);
+      EXECUTE Format('INSERT INTO %s(line_geo_lost, error_info, d_state, d_msg, d_detail, d_hint, d_context, geo) VALUES(%L, %L, %L, %L, %L, %L, %L, %L)', 
+      no_cutline_filename, FALSE, 'Warn1, will do retry, topo_update.add_border_lines ', v_state, v_msg, v_detail, v_hint, v_context, _new_line);
       BEGIN
-        PERFORM topology.TopoGeo_addLinestring(_topology_name,geom,_snap_tolerance) 
-        FROM (
-          SELECT (ST_Dump (geom)).geom
-          FROM (
-            SELECT topo_update.get_single_lineparts ((ST_Dump (topo_update.extend_line(_new_line, _snap_tolerance*2))).geom) AS geom) AS r) AS r
-          WHERE ST_length (geom) > 0;  
+        single_line_geo = ST_Multi(topo_update.get_single_lineparts(_new_line));
+         
+        SELECT ST_NumGeometries(single_line_geo) INTO dim;
+      WHILE i < dim LOOP
+        i:=i+1;  
+        new_egde_geom := ST_GeometryN(single_line_geo, i);
+---
+BEGIN
+   	command_string := Format('select topology.TopoGeo_addLinestring(%s,%L,%s)', Quote_literal(_topology_name), new_egde_geom, _snap_tolerance);
+    EXECUTE command_string;
+    EXCEPTION
+    WHEN OTHERS THEN
+      RAISE NOTICE 'failed topo_update.add_border_lines ::::::::::::::::::::::::::::::::::::::::::::::::::: %', ST_GeometryType (new_egde_geom);
+      GET STACKED DIAGNOSTICS v_state = RETURNED_SQLSTATE, v_msg = MESSAGE_TEXT, v_detail = PG_EXCEPTION_DETAIL, v_hint = PG_EXCEPTION_HINT,
+      v_context = PG_EXCEPTION_CONTEXT;
+      RAISE NOTICE 'failed: state  : % message: % detail : % hint   : % context: %', v_state, v_msg, v_detail, v_hint, v_context;
+      EXECUTE Format('INSERT INTO %s(line_geo_lost, error_info, d_state, d_msg, d_detail, d_hint, d_context, geo) VALUES(%L, %L, %L, %L, %L, %L, %L, %L)', 
+      no_cutline_filename, FALSE, 'Warn2, will do retry, topo_update.add_border_lines ', v_state, v_msg, v_detail, v_hint, v_context, new_egde_geom);
+--1
+
+      BEGIN
+	      new_egde_geom := topo_update.extend_line(new_egde_geom, _snap_tolerance*-2);
+   	command_string := Format('select topology.TopoGeo_addLinestring(%s,%L,%s)', Quote_literal(_topology_name), new_egde_geom, _snap_tolerance);
+    EXECUTE command_string;
+    EXCEPTION
+    WHEN OTHERS THEN
+      RAISE NOTICE 'failed topo_update.add_border_lines ::::::::::::::::::::::::::::::::::::::::::::::::::: %', ST_GeometryType (new_egde_geom);
+      GET STACKED DIAGNOSTICS v_state = RETURNED_SQLSTATE, v_msg = MESSAGE_TEXT, v_detail = PG_EXCEPTION_DETAIL, v_hint = PG_EXCEPTION_HINT,
+      v_context = PG_EXCEPTION_CONTEXT;
+      RAISE NOTICE 'failed: state  : % message: % detail : % hint   : % context: %', v_state, v_msg, v_detail, v_hint, v_context;
+      EXECUTE Format('INSERT INTO %s(line_geo_lost, error_info, d_state, d_msg, d_detail, d_hint, d_context, geo) VALUES(%L, %L, %L, %L, %L, %L, %L, %L)', 
+      no_cutline_filename, TRUE, 'Failed1, topo_update.add_border_lines', v_state, v_msg, v_detail, v_hint, v_context, new_egde_geom);
+      
+END ;    
+
+--1      
+      
+END ;    
+        
+        
+---        
+        
+      END LOOP;
         EXCEPTION
         WHEN OTHERS THEN
           GET STACKED DIAGNOSTICS v_state = RETURNED_SQLSTATE, v_msg = MESSAGE_TEXT, v_detail = PG_EXCEPTION_DETAIL, v_hint = PG_EXCEPTION_HINT,
           v_context = PG_EXCEPTION_CONTEXT;
         RAISE NOTICE 'failed: state  : % message: % detail : % hint   : % context: %', v_state, v_msg, v_detail, v_hint, v_context;
-        EXECUTE Format('INSERT INTO %s(line_geo_lost, error_info, d_state, d_msg, d_detail, d_hint, d_context, geo) VALUES(%L, %L, %L, %L, %L, %L, %L, %L)', _table_name_result_prefix || '_no_cut_line_failed', TRUE, 'Failed topo_update.add_border_lines ', v_state, v_msg, v_detail, v_hint, v_context, _new_line);
-        END;
+        EXECUTE Format('INSERT INTO %s(line_geo_lost, error_info, d_state, d_msg, d_detail, d_hint, d_context, geo) VALUES(%L, %L, %L, %L, %L, %L, %L, %L)', 
+        no_cutline_filename, TRUE, 'Failed2, topo_update.add_border_lines ', v_state, v_msg, v_detail, v_hint, v_context, _new_line);
       END;
     END;
   RETURN update_egde_id;
@@ -93,14 +87,4 @@ END;
 
 $$
 LANGUAGE plpgsql;
-
-UPDATE
-  test_topo_jm.jm_ukomm_flate_problem_job_list
-SET block_bb = NULL;
-
-TRUNCATE test_topo_jm.jm_ukomm_flate_problem_no_cut_line_failed;
-
-TRUNCATE test_topo_jm.jm_ukomm_flate_problem_job_list_donejobs;
-
-CALL resolve_overlap_gap_single_cell ('test_data.jm_ukomm_flate_problem', 'geo', 'figurid', 'test_topo_jm.jm_ukomm_flate_problem', 'test_topo_jm', 4258, 'false', 2e-06, 1e-06, 'false', 49, 'test_topo_jm.jm_ukomm_flate_problem_job_list', 'test_topo_jm.jm_ukomm_flate_problem_grid', '0103000020A21000000100000005000000019C3592DD2B2640C2A4F8F884C24F40019C3592DD2B2640328E91EC91CE4F4094AA6F9DC0602640328E91EC91CE4F4094AA6F9DC0602640C2A4F8F884C24F40019C3592DD2B2640C2A4F8F884C24F40', 1);
 

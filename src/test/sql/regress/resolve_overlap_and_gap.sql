2c6c99bcb6a090e31234f0f3c4e3b28800a9b866
Lars Aksel Opsahl <lop@skogoglandskap.no>
2020-03-13 14:22:17 +0100

    Test with simplify parameters set

--- src/test/sql/regress/resolve_overlap_and_gap.sql
+++ src/test/sql/regress/resolve_overlap_and_gap.sql
@@ -11,7 +11,6 @@ CALL resolve_overlap_gap_run(
 ('test_data.overlap_gap_input_t2','c1','geom',4258,false), -- TYPE resolve_overlap_data_input
 ('test_topo_t2',0.00001), -- TYPE resolve_overlap_data_topology
 resolve_overlap_data_clean_type_func(  -- TYPE resolve_overlap_data_clean
-
 49,
 0,
 false, -- if false the parameters below are used.
@@ -45,7 +44,15 @@ SELECT 'degrees', topology.droptopology('test_topo_t2');
 CALL resolve_overlap_gap_run(
 ('test_data.overlap_gap_input_t3','c1t3','geo',25833,true), -- TYPE resolve_overlap_data_input
 ('test_topo_t3',1.0), -- TYPE resolve_overlap_data_topology
-resolve_overlap_data_clean_type_func(49), -- TYPE resolve_overlap_data_clean
+resolve_overlap_data_clean_type_func(  -- TYPE resolve_overlap_data_clean
+49,
+30,
+true, -- if false the parameters below are used.
+10000, --edge that are longer than this value will not be touched, The basic idea idea is to use smooth out sharp edges in another way than    
+120,  -- The angle has to be less this given value, This is used to avoid to touch all angles. 
+240, -- The angle has to be greather than this given value, This is used to avoid to touch all angles 
+1 -- A big value here make no sense because the number of points will increaes exponential )
+),
 5,4);
 
 SELECT 'utm_check_failed_lines', count(geo) from test_topo_t3.overlap_gap_input_t3_no_cut_line_failed;

cb13841d5db12ee48e8e7d2eaf63fc675081a1de
Lars Aksel Opsahl <lop@skogoglandskap.no>
2020-07-26 14:31:55 +0200

    use uniqe temp table name to make debug easier

--- src/main/extern_pgtopo_update_sql/view_get_simplified_border_lines_function_v.sql
+++ src/main/extern_pgtopo_update_sql/view_get_simplified_border_lines_function_v.sql
@@ -26,6 +26,8 @@ DECLARE
   
   cell_id int;
   
+  tmp_table_name varchar = 'tmp_data_all_lines' || Md5(ST_AsBinary (_bb));
+  
 BEGIN
 	
 
@@ -43,10 +45,10 @@ BEGIN
        SELECT ST_ExteriorRing (inner_boundary_geom) AS inner_rings));
 
   
-  RAISE NOTICE 'removed _bb %  ',  ST_AsText(_bb);
+  RAISE NOTICE 'enter topo_update.get_simplified_border_lines with _bb %  ',  ST_AsText(_bb);
   
   --_bb := _bb;
-  command_string := Format('CREATE TEMP TABLE tmp_data_all_lines AS WITH 
+  command_string := Format('CREATE TEMP TABLE %8$s AS WITH 
     rings AS (
  	  SELECT ST_ExteriorRing((ST_DumpRings((st_dump(%3$s)).geom)).geom) as geom
  	  FROM %1$s v
@@ -92,37 +94,46 @@ BEGIN
  	_topology_snap_tolerance,
  	_table_name_result_prefix||'_grid', 
  	ST_ExteriorRing(_bb),
- 	_topology_snap_tolerance
+ 	_topology_snap_tolerance,
+ 	tmp_table_name
  	);
   EXECUTE command_string;
 
   
  
-  command_string := Format('create index on tmp_data_all_lines using gist(geom)', 'idxtmp_data_all_lines_geom' || Md5(ST_AsBinary (_bb)));
+  command_string := Format('create index on %2$s using gist(geom)', 
+  'idxtmp_data_all_lines_geom' || Md5(ST_AsBinary (_bb)),
+  tmp_table_name);
   EXECUTE command_string;
   
-  command_string := Format('create index on tmp_data_all_lines(min_cell_id)', 'idxtmp_data_all_lines_min_cell_id' || Md5(ST_AsBinary (_bb)));
+  command_string := Format('create index on %2$s(min_cell_id)', 
+  'idxtmp_data_all_lines_min_cell_id' || Md5(ST_AsBinary (_bb)),
+  tmp_table_name );
   EXECUTE command_string;
 
    
   -- log error lines
   EXECUTE Format('WITH error_lines AS 
-    (DELETE FROM tmp_data_all_lines r where r.min_cell_id = %1$s and ST_IsValid(r.geom) = false RETURNING geom) 
+    (DELETE FROM %4$s r where r.min_cell_id = %1$s and ST_IsValid(r.geom) = false RETURNING geom) 
     INSERT INTO %2$s (error_info, geo)
     SELECT %3$L AS error_info, r.geom as geo
     FROM error_lines r',
     cell_id,
-   _table_name_result_prefix||'_no_cut_line_failed',
-   'Failed to make valid input border line for tmp_boundary_lines_merged' );
+    _table_name_result_prefix||'_no_cut_line_failed',
+    'Failed to make valid input border line for tmp_boundary_lines_merged',
+ 	tmp_table_name );
   
  
 
   -- insert lines with more than max point
   EXECUTE Format('WITH long_lines AS 
-    (DELETE FROM tmp_data_all_lines r where npoints >  %2$s and ST_Intersects(geom,%3$L) and min_cell_id = %4$s RETURNING geom) 
-    INSERT INTO %1$s (geo) SELECT distinct (ST_dump(geom)).geom as geo from long_lines'
-  ,_table_name_result_prefix||'_border_line_many_points', _max_point_in_line, 
-  ST_ExteriorRing(_bb), cell_id);
+    (DELETE FROM %5$s r where npoints >  %2$s and ST_Intersects(geom,%3$L) and min_cell_id = %4$s RETURNING geom) 
+    INSERT INTO %1$s (geo) SELECT distinct (ST_dump(geom)).geom as geo from long_lines',
+    _table_name_result_prefix||'_border_line_many_points',
+    _max_point_in_line, 
+    ST_ExteriorRing(_bb), 
+    cell_id,
+ 	tmp_table_name);
 
     
  
@@ -130,23 +141,34 @@ BEGIN
  
   EXECUTE Format('INSERT INTO %s (geo, point_geo)
   SELECT l.geom as geo, NULL AS point_geo
-  FROM tmp_data_all_lines l where ST_Intersects(geom,%2$L) and min_cell_id = %3$s and ST_IsValid(l.geom) = true '
-  ,_table_name_result_prefix||'_border_line_segments', boundary_geom, cell_id );
+  FROM %4$s l where ST_Intersects(geom,%2$L) and min_cell_id = %3$s and ST_IsValid(l.geom) = true ',
+  _table_name_result_prefix||'_border_line_segments', 
+  boundary_geom,
+  cell_id,
+  tmp_table_name);
+
+  RAISE NOTICE 'done with %s' ,  tmp_table_name;
 
       
   -- return the result of inner geos to handled imediatly
-  RETURN QUERY
-  SELECT * FROM (
+  
+  command_string := Format('SELECT * FROM (
     SELECT l.geom as geo, false as outer_border_line FROM
-    tmp_data_all_lines l WHERE  ST_CoveredBy(l.geom,inner_boundary_geom)
+    %1$s l WHERE  ST_CoveredBy(l.geom,%2$L)
     union 
     SELECT ST_Intersection(ST_Union(
     ST_StartPoint(out.geom),
-    ST_EndPoint(out.geom)),_bb) as geo
+    ST_EndPoint(out.geom)),%3$L) as geo
     , true as outer_border_line FROM
-    tmp_data_all_lines out where ST_Intersects(out.geom,boundary_geom)
- 
-  ) AS f;
+    %1$s out where ST_Intersects(out.geom,%4$L)
+  ) AS f',
+  tmp_table_name, 
+  inner_boundary_geom,
+  _bb,
+  boundary_geom);
+  
+  
+  RETURN QUERY EXECUTE command_string;
 END
 $function$;
 

601731141770fab2cdea4cf78c81a60c52078e21
Lars Aksel Opsahl <lop@skogoglandskap.no>
2022-03-05 09:53:31 +0100

    Make it more generic

--- src/main/extern_pgtopo_update_sql/utils/function_01_topo_touches.sql
+++ src/main/extern_pgtopo_update_sql/utils/function_01_topo_touches.sql
@@ -6,15 +6,17 @@
 -- DROP FUNCTION IF EXISTS topo_update.touches(_new_topo_objects regclass,id_to_check int) ;
 
 DROP FUNCTION IF EXISTS topo_update.touches(
-	_new_topo_objects regclass, 
-	id_to_check int,
-	surface_topo_info topo_update.input_meta_info
+	_face_attributes regclass,
+	_face_attributes_pk_column_name text,  
+	_face_attributes_topology_column_name text,  
+	_surface_topo_info topo_update.input_meta_info
 );
 
 CREATE OR REPLACE FUNCTION topo_update.touches(
-	_new_topo_objects regclass, 
-	id_to_check int,
-	surface_topo_info topo_update.input_meta_info
+	_face_attributes regclass,
+	_face_attributes_pk_column_name text,  
+	_face_attributes_topology_column_name text,  
+	_surface_topo_info topo_update.input_meta_info
 ) 
 RETURNS TABLE (
 topo_object_to_expand text,
@@ -28,18 +30,19 @@ command_string text;
 
 BEGIN
 
-RETURN QUERY SELECT 
+
+command_string := FORMAT('SELECT 
 CASE 
       WHEN r.f1_mbr > r.f2_mbr  THEN r.gid1
       ELSE r.gid2
 END::text AS topo_object_to_expand,
 CASE 
-      WHEN r.f1_mbr < r.f2_mbr  THEN gid2
+      WHEN r.f1_mbr > r.f2_mbr  THEN r.gid2
       ELSE r.gid1
 END::text AS topo_object_to_remove,
 r.edge_id_to_remove
 FROM (
-SELECT face_attr_01.gid AS gid1, face_attr_02.gid AS gid2, fa.edge_id as edge_id_to_remove, fa.mbr_area, f1_mbr, f2_mbr 
+SELECT face_attr_01.%3$s AS gid1, face_attr_02.%3$s AS gid2, fa.edge_id as edge_id_to_remove, fa.mbr_area, f1_mbr, f2_mbr 
 FROM 
 (
 	SELECT DISTINCT ON (f1_face_id) f1_face_id, f2_face_id, edge_id, mbr_area, f1_mbr, f2_mbr
@@ -49,9 +52,9 @@ FROM
 		f2.face_id f2_face_id, ST_Area(f2.mbr) AS f2_mbr,
 		e1.edge_id, (ST_Area(f1.mbr) + ST_Area(f2.mbr)) mbr_area
 		FROM
-		test_ar50_flate_lars_06.face f1,
-		test_ar50_flate_lars_06.face f2,
-		test_ar50_flate_lars_06.edge e1
+		%2$s.face f1,
+		%2$s.face f2,
+		%2$s.edge e1
 		WHERE 
 		f1.face_id > 0 AND 
 		f2.face_id > 0 AND 
@@ -59,24 +62,37 @@ FROM
 	) r
 	ORDER BY f1_face_id, mbr_area
 ) fa,
-test_ar50_flate_lars_06.relation r1,
-test_ar50_flate_lars_06.relation r2,
-test_ar50_flate_lars_06.face_attributes face_attr_01,
-test_ar50_flate_lars_06.face_attributes face_attr_02
-WHERE fa.f1_face_id = r1.element_id AND r1.element_type = 3 AND r1.layer_id = 2
-AND fa.f2_face_id = r2.element_id AND r2.element_type = 3 AND r2.layer_id = 2
-AND r1.topogeo_id = ((face_attr_01.geo).id)
-AND r2.topogeo_id = ((face_attr_02.geo).id)
-AND face_attr_01.gid != face_attr_02.gid
-) r ORDER BY mbr_area;
+%2$s.relation r1,
+%2$s.relation r2,
+%1$s face_attr_01,
+%1$s face_attr_02
+WHERE fa.f1_face_id = r1.element_id AND r1.element_type = %6$L AND r1.layer_id = %5$L
+AND fa.f2_face_id = r2.element_id AND r2.element_type = %6$L AND r2.layer_id = %5$L
+AND r1.topogeo_id = ((face_attr_01.%4$s).id)
+AND r2.topogeo_id = ((face_attr_02.%4$s).id)
+AND face_attr_01.%3$s != face_attr_02.%3$s
+) r ORDER BY mbr_area',
+_face_attributes,
+_surface_topo_info.topology_name,
+_face_attributes_pk_column_name,
+_face_attributes_topology_column_name,
+_surface_topo_info.border_layer_id,
+_surface_topo_info.element_type    
+--
+);
+
+RETURN QUERY EXECUTE command_string;
 
 END
 $$ LANGUAGE plpgsql;
 
 
  
-SELECT * FROM topo_update.touches('test_ar50_flate_lars_06.face_attributes',80724,
+SELECT * FROM topo_update.touches('test_ar50_flate_lars_06.face_attributes',
+'gid',
+'geo',
 '("test_ar50_flate_lars_06","a","b","geo",3,0.1,2,25833)'
 );
+
 
 

ed600324ab3e8d61f19eef5bdbc2aacd16c930c6
Lars Aksel Opsahl <lop@skogoglandskap.no>
2022-03-06 09:30:23 +0100

    Made function more generic

--- src/main/extern_pgtopo_update_sql/utils/function_01_merge_topo.sql
+++ src/main/extern_pgtopo_update_sql/utils/function_01_merge_topo.sql
@@ -2,18 +2,29 @@
 -- Remove a edge between to topo objects and clean up
 
 DROP FUNCTION IF EXISTS topo_update.merge_topo(
-	_topo_objects_table regclass, 
-	_topo_object_to_expand int,
-	_topo_object_to_remove int,
+	_face_attribute_table regclass, 
+	_face_attribute_to_expand int,
+	_face_attribute_to_remove int,
 	_edge_id_to_remove int,
 	_surface_topo_info topo_update.input_meta_info
 ) ;
 
+DROP FUNCTION IF EXISTS topo_update.merge_topo(
+	_face_attribute_table regclass, 
+	_face_attribute_pk_column_name text,  
+	_face_attribute_topology_column_name text,  
+	_face_attribute_to_expand int,
+	_face_attribute_to_remove int,
+	_edge_id_to_remove int,
+	_surface_topo_info topo_update.input_meta_info
+) ;
 
 CREATE OR REPLACE FUNCTION topo_update.merge_topo(
-	_topo_objects_table regclass, 
-	_topo_object_to_expand int,
-	_topo_object_to_remove int,
+	_face_attribute_table regclass, 
+	_face_attribute_pk_column_name text,  
+	_face_attribute_topology_column_name text,  
+	_face_attribute_to_expand int,
+	_face_attribute_to_remove int,
 	_edge_id_to_remove int,
 	_surface_topo_info topo_update.input_meta_info
 ) 
@@ -25,10 +36,11 @@ found_topo_object int;
 topo_object_edge int;
 BEGIN
 
-command_string := format('SELECT count(*) FROM %1$s f_attr WHERE f_attr.gid in (%2$L, %3$L)',
-_topo_objects_table,
-_topo_object_to_remove,
-_topo_object_to_expand
+command_string := format('SELECT count(*) FROM %1$s f_attr WHERE f_attr.%4$s in (%2$L, %3$L)',
+_face_attribute_table,
+_face_attribute_to_remove,
+_face_attribute_to_expand,
+_face_attribute_pk_column_name
 );
 EXECUTE command_string INTO found_topo_object ;
 IF found_topo_object != 2 THEN
@@ -46,18 +58,21 @@ IF topo_object_edge = 0 THEN
 	RETURN face_to_use;
 END IF;
 
-command_string := format('SELECT topology.clearTopoGeom(f_attr.geo)
+command_string := format('SELECT topology.clearTopoGeom(f_attr.%4$s)
 FROM %1$s f_attr
-WHERE f_attr.gid in (%2$L, %3$L)',
-_topo_objects_table,
-_topo_object_to_remove,
-_topo_object_to_expand
+WHERE f_attr.%5$s in (%2$L, %3$L)',
+_face_attribute_table,
+_face_attribute_to_remove,
+_face_attribute_to_expand,
+_face_attribute_topology_column_name,
+_face_attribute_pk_column_name
 );
 EXECUTE command_string;
 
-command_string := format('delete from %1$s f_attr WHERE f_attr.gid = %2$L',
-_topo_objects_table,
-_topo_object_to_remove
+command_string := format('delete from %1$s f_attr WHERE f_attr.%3$s = %2$L',
+_face_attribute_table,
+_face_attribute_to_remove,
+_face_attribute_pk_column_name
 );
 EXECUTE command_string;
 
@@ -68,16 +83,18 @@ _edge_id_to_remove
 EXECUTE command_string INTO face_to_use;
  
 command_string := format('UPDATE %1$s f_attr
-set geo = r.geo
+set %5$s = r.%5$s
 FROM 
-(SELECT topology.CreateTopoGeom(%2$L,3,2, topology.TopoElementArray_Agg(ARRAY[f.face_id,3])) AS geo
+(SELECT topology.CreateTopoGeom(%2$L,3,2, topology.TopoElementArray_Agg(ARRAY[f.face_id,3])) AS %5$s
 FROM %2$s.face f where face_id = %3$s) 
 as r
-WHERE f_attr.gid in (%4$s)',
-_topo_objects_table,
-_surface_topo_info.topology_name,
-face_to_use,
-_topo_object_to_expand
+WHERE f_attr.%6$s in (%4$s)',
+_face_attribute_table, --1
+_surface_topo_info.topology_name, --2
+face_to_use, --3
+_face_attribute_to_expand, --4
+_face_attribute_topology_column_name, --5
+_face_attribute_pk_column_name --6
 );
 EXECUTE command_string;
 
@@ -108,10 +125,7 @@ $$ LANGUAGE plpgsql;
 --\timing
 
  
---SELECT * FROM topo_update.merge_topo('test_ar50_flate_lars_06.face_attributes',80724,79285,3573,'("test_ar50_flate_lars_06","a","b","geo",3,0.1,2,25833)');
---SELECT * FROM topo_update.merge_topo('test_ar50_flate_lars_06.face_attributes',3579,79681,79704,'("test_ar50_flate_lars_06","a","b","geo",3,0.1,2,25833)');
---SELECT * FROM topo_update.merge_topo('test_ar50_flate_lars_06.face_attributes',3580,79285,79704,'("test_ar50_flate_lars_06","a","b","geo",3,0.1,2,25833)');
---SELECT * FROM topo_update.merge_topo('test_ar50_flate_lars_06.face_attributes',2657,79314,79336,'("test_ar50_flate_lars_06","a","b","geo",3,0.1,2,25833)');
---SELECT * FROM topo_update.merge_topo('test_ar50_flate_lars_06.face_attributes',3536,79295,79336,'("test_ar50_flate_lars_06","a","b","geo",3,0.1,2,25833)');
-
+SELECT * FROM topo_update.merge_topo('test_ar50_flate_lars_06.face_attributes','gid','geo'
+,79716,79713,3209,
+'("test_ar50_flate_lars_06","a","b","geo",3,0.1,2,25833)');
 
